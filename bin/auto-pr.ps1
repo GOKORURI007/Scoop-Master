a518578825c649cef1e8d74814c78c8c0c6461cd
GokoRuri <yjc.goukou@foxmail.com>
2024-06-19 18:57:04 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "GOUKOU007/Scoop-Master:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
